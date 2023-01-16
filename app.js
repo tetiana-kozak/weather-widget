23eb5e13fe2fb9ccf2769c024764bea4986483d8
Tetiana <kozaktanya077@gmail.com>
2023-01-16 22:44:49 +0200

    add: first data from API to page

---
+++ app.js
@@ -0,0 +1,33 @@
+function getWeatherData() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("data :>> ", data);
+      setWeatherData(data);
+    });
+}
+
+function setWeatherData(data) {
+  const city = document.querySelector(".city");
+  const icon = document.querySelector(".icon");
+  const temp = document.querySelector(".temp");
+  const weatherType = document.querySelector(".weather-type");
+
+  city.innerHTML = data.name;
+
+  if (data.weather[0].icon === "10d") {
+    icon.setAttribute("src", "https://openweathermap.org/img/w/10d.png");
+  } else {
+    icon.setAttribute("src", "https://openweathermap.org/img/w/10n.png");
+  }
+
+  temp.innerHTML = Math.round(data.main.temp);
+  weatherType.innerHTML = data.weather[0].description;
+  // console.log("data.name :>> ", data.name);
+  // console.log("data.temp :>> ", Math.round(data.main.temp));
+  // console.log("data.weather.main :>> ", data.weather[0].main);
+}
+
+getWeatherData();
